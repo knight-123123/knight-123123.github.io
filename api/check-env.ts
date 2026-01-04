054c0705e2930a81b488d8fde732f620f9ab0c03
knight-123123 <knight-123123@users.noreply.github.com>
2026-01-04 02:19:08 +0000

    deploy: 85deeef6ebc9d888d14574a14d4b1dcdb9aa9843

--- api/check-env.ts
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  res.json({
-    hasClientId: Boolean(process.env.GITHUB_CLIENT_ID),
-    hasClientSecret: Boolean(process.env.GITHUB_CLIENT_SECRET),
-  });
-}
